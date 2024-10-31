9156811b16f1fa5d541fd21e590e420652617342
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-10-30 21:06:22 -0300

    :sparkles: feat: Initial implementation of the recipe server

---
+++ recipes-api/data/recipes.js
@@ -0,0 +1,16 @@
+let recipes = [
+  {
+    id: 1,
+    name: 'Bolo de Chocolate',
+    ingredients: ['farinha', 'açúcar', 'chocolate em pó'],
+    type: 'sobremesa'
+  },
+  {
+    id: 2,
+    name: 'Salada Caesar',
+    ingredients: ['alface', 'croutons', 'parmesão'],
+    type: 'entrada'
+  },
+];
+
+module.exports = recipes;
